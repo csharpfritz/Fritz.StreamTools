d0e6cb75842505f0269878d4a7476381fe45c0ec
Jeff Fritz <jeffreytfritz@gmail.com>
2017-12-16 12:17:03 -0500

    Now reading Twitch followers and using settings for Twitch client

---
+++ Fritz.RunDown/Controllers/FollowersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fritz.RunDown.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fritz.RunDown.Controllers
+{
+  public class FollowersController : Controller
+  {
+
+    public FollowersController(TwitchService twitch)
+    {
+      this.TwitchService = twitch;
+    }
+
+    public TwitchService TwitchService { get; }
+
+    public int Index()
+    {
+      return TwitchService.CurrentFollowerCount;
+    }
+  }
+}

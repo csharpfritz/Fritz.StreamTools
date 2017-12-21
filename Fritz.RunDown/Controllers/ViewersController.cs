d01550575a222611881f07c49f01f019757de340
Jeff Fritz <jeffreytfritz@gmail.com>
2017-12-21 11:15:37 -0500

    Added current viewers data collection

---
+++ Fritz.RunDown/Controllers/ViewersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fritz.RunDown.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fritz.RunDown.Controllers
+{
+  public class ViewersController : Controller
+  {
+
+    public ViewersController(TwitchService twitch, MixerService mixer)
+    {
+      this.Twitch = twitch;
+      this.Mixer = mixer;
+    }
+
+    public TwitchService Twitch { get; }
+    public MixerService Mixer { get; }
+
+    public IActionResult Current()
+    {
+      return View(Twitch.CurrentViewerCount + Mixer.CurrentViewerCount);
+    }
+  }
+}
--- Fritz.RunDown/Services/TwitchService.cs
+++ Fritz.RunDown/Services/TwitchService.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using TwitchLib.Models.API.v5.Streams;
 using TwitchLib.Services;
 
 namespace Fritz.RunDown.Services
@@ -37,19 +39,44 @@ namespace Fritz.RunDown.Services
     public static int _CurrentFollowerCount;
     public int CurrentFollowerCount { get { return _CurrentFollowerCount; } }
 
+    public static int _CurrentViewerCount;
+    private Timer _Timer;
+    private StreamByUser _Stream;
+
+    public int CurrentViewerCount {  get { return _CurrentViewerCount; } }
+
+    private string ClientId { get { return Configuration["StreamServices:Twitch:ClientId"];  }  }
+
+    private string Channel { get { return Configuration["StreamServices:Twitch:Channel"]; } }
+
+    private string ChannelId {  get{ return Configuration["StreamServices:Twitch:UserId"]; } }
+
 
     private async Task StartTwitchMonitoring()
     {
-      var api = new TwitchLib.TwitchAPI(clientId: Configuration["StreamServices:Twitch:ClientId"]);
+      var api = new TwitchLib.TwitchAPI(clientId: ClientId);
       Service = new FollowerService(api);
-      Service.SetChannelByName(Configuration["StreamServices:Twitch:Channel"]);
+      Service.SetChannelByName(Channel);
       await Service.StartService();
 
       var v5 = new TwitchLib.Channels.V5(api);
-      var follows = await v5.GetAllFollowersAsync(Configuration["StreamServices:Twitch:UserId"]);
+      var follows = await v5.GetAllFollowersAsync(ChannelId);
+      var viewers = 
 
       _CurrentFollowerCount = follows.Count;
       Service.OnNewFollowersDetected += Service_OnNewFollowersDetected;
+
+      _Stream = await api.Streams.v5.GetStreamByUserAsync(ChannelId);
+      _Timer = new Timer(UpdateViewers, null, 0, 5000);
+
+    }
+
+    private void UpdateViewers(object state)
+    {
+      
+      var count = _Stream.Stream.Viewers;
+      Interlocked.Exchange(ref _CurrentViewerCount, count);
+
 
     }
 

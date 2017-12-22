20e910212f5b1268c462aa5145573067b9e9e732
Jeff Fritz <jeffreytfritz@gmail.com>
2017-12-22 10:07:02 -0500

    Updated build.cmd to create docker image and publish to private repository

--- build.cmd
+++ build.cmd
@@ -1,1 +1,6 @@
 docker build -t fritz.rundown:%1 -t fritz.rundown:latest -f Fritz.RunDown\Dockerfile .
+
+docker tag fritz.rundown:%1 fritzregistry.azurecr.io/fritz.rundown:%1
+docker tag fritz.rundown:%1 fritzregistry.azurecr.io/fritz.rundown:latest
+
+docker push fritzregistry.azurecr.io/fritz.rundown

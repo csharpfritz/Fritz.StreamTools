17dfc92e2cf4bfc641d3724a75045f4c706fd2f0
staker4 <staker4@gmail.com>
2018-01-21 00:36:59 -0500

    refactor: pass SignalrTagHelperOptions to SignalrTagHelper constructor to configure the src attribute
    Passing options to the TagHelper keeps it's responsibility to only generate a script tag.
    Finding the client library source is delegated to the DI services configuration.
    
    - Make use of IConfigureOptions<SignalrTagHelperOptions> to facilitate DI configuration.
    - Add unit tests on SignalrTagHelper.Process().
    
    fixes #24

---
+++ Fritz.StreamTools/StartupServices/ConfigureSignalrTagHelperOptions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Fritz.StreamTools.TagHelpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Fritz.StreamTools.StartupServices
+{
+
+	public class ConfigureSignalrTagHelperOptions : IConfigureOptions<SignalrTagHelperOptions>
+	{
+
+		private readonly IHostingEnvironment Env;
+		private readonly ILogger Logger;
+
+		public ConfigureSignalrTagHelperOptions(IHostingEnvironment env, ILogger<ConfigureSignalrTagHelperOptions> logger)
+		{
+
+			Env = env;
+			Logger = logger;
+
+		}
+
+		public void Configure(SignalrTagHelperOptions options)
+		{
+
+			var folder = new DirectoryInfo(Path.Combine(Env.WebRootPath, "lib", "signalr"));
+
+			var fileInfo = folder.Exists
+				? folder.GetFiles("signalr-client-*.min.js").OrderByDescending(f => f.Name).FirstOrDefault()
+				: null;
+
+			if (fileInfo == null)
+			{
+				var error = "Required signalr-client library not found.";
+				Logger.LogCritical(error);
+				throw new InvalidOperationException(error);
+			}
+
+			options.ClientLibarySource = $"/lib/signalr/{fileInfo.Name}";
+
+		}
+
+	}
+
+}
--- Fritz.StreamTools/TagHelpers/SignalrTagHelper.cs
+++ Fritz.StreamTools/TagHelpers/SignalrTagHelper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Abstractions;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+﻿using Fritz.StreamTools.StartupServices;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Fritz.StreamTools.TagHelpers
@@ -15,38 +8,22 @@ namespace Fritz.StreamTools.TagHelpers
 	public class SignalrTagHelper : TagHelper
 	{
 
-		public SignalrTagHelper(IHostingEnvironment env) {
+		private readonly SignalrTagHelperOptions Options;
 
-			this.HostingEnvironment = env;
-
+		public SignalrTagHelper(SignalrTagHelperOptions options)
+		{
+			Options = options;
 		}
-
-		private readonly IHostingEnvironment HostingEnvironment;
 
 		public override void Process(TagHelperContext context, TagHelperOutput output)
 		{
 
 			output.TagName = "script";
 			output.TagMode = TagMode.StartTagAndEndTag;
-
-			var filename = IdentifyClientLibrary(new FileSystem(), HostingEnvironment.WebRootPath);
-
-			output.Attributes.Add("src", filename);
+			output.Attributes.Add("src", Options.ClientLibarySource);
 
 		}
 
-		internal string IdentifyClientLibrary(IFileSystem fileSystem, string webRootPath)
-		{
-
-			var folderName = fileSystem.Path.Combine(webRootPath, "lib/signalr");
-			var folder = fileSystem.DirectoryInfo.FromDirectoryName(folderName);
-
-			var fileInfo = folder.GetFiles("signalr-client-*.min.js")
-				.OrderByDescending(f => f.Name).First();
-
-			return $"~/lib/signalr/{fileInfo.Name}";
-
-
-		}
 	}
+
 }

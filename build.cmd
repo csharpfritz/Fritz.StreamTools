2ca9bf1092aa40628e5c9318a819e9e3c44d2abd
Jeff Fritz <jeffreytfritz@gmail.com>
2018-01-27 12:39:17 -0500

    Added git tag to build process

--- build.cmd
+++ build.cmd
@@ -3,4 +3,7 @@ docker build -t fritz.streamtools:%1 -t fritz.streamtools:latest -f Fritz.Stream
 docker tag fritz.streamtools:%1 fritzregistry.azurecr.io/fritz.streamtools:%1
 docker tag fritz.streamtools:%1 fritzregistry.azurecr.io/fritz.streamtools:latest
 
+git tag v%1
+git push --tags
+
 docker push fritzregistry.azurecr.io/fritz.streamtools

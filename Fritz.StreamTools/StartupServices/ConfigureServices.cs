f4e55be8c8c659eec0d8ebf810a058844571022d
Jeff Fritz <jeffreytfritz@gmail.com>
2018-02-03 11:27:13 -0500

    Added some comments

--- Fritz.StreamTools/StartupServices/ConfigureServices.cs
+++ Fritz.StreamTools/StartupServices/ConfigureServices.cs
@@ -26,27 +26,42 @@ namespace Fritz.StreamTools.StartupServices
 			IConfiguration configuration)
 		{		
 			services.ConfigureStreamService(configuration, 
-				(c, l) => new TwitchService(c, l), 
-				c => string.IsNullOrEmpty(c["StreamServices:Twitch:ClientId"]));
+				(c, l) => new TwitchService(c, l),																	// Factory
+				c => string.IsNullOrEmpty(c["StreamServices:Twitch:ClientId"]));		// Test to disable
 			services.ConfigureStreamService(configuration, 
-				(c, l) => new MixerService(c, l), 
-				c => string.IsNullOrEmpty(c["StreamServices:Mixer:ClientId"]));
+				(c, l) => new MixerService(c, l),                                   // Factory
+				c => string.IsNullOrEmpty(c["StreamServices:Mixer:ClientId"]));			// Test to disable
 			services.ConfigureStreamService(configuration, 
-				(c, l) => new FakeService(c, l), 
-				c => !bool.TryParse(c["StreamServices:Fake:Enabled"], out var enabled) || !enabled);
+				(c, l) => new FakeService(c, l),                                                          // Factory
+				c => !bool.TryParse(c["StreamServices:Fake:Enabled"], out var enabled) || !enabled);			// Test to disable
 			
 			services.AddSingleton<StreamService>();	
 		}
 
+		/// <summary>
+		/// Generically configure stream service providers and register them with the DI container
+		/// </summary>
+		/// <typeparam name="TStreamService">Type of StreamService to create</typeparam>
+		/// <param name="services">The DI services configuration object</param>
+		/// <param name="configuration">Application Configuration to use to populate our service</param>
+		/// <param name="factory">Callback method that defines how to instantiate the service</param>
+		/// <param name="isDisabled">Callback test to determine whether to disable the service</param>
 		private static void ConfigureStreamService<TStreamService>(this IServiceCollection services, 
-			IConfiguration configuration, Func<IConfiguration, ILoggerFactory, TStreamService> factory, 
+			IConfiguration configuration,
+			Func<IConfiguration, ILoggerFactory, TStreamService> factory, 
 			Func<IConfiguration, bool> isDisabled) 
-			where TStreamService : class
+			where TStreamService : class, IStreamService
 		{
-			if(isDisabled(configuration))
+
+			// Don't configure this service if it is disabled
+			if (isDisabled(configuration))
+			{
 				return;
-			
-			var provider = services.BuildServiceProvider();
+			}
+
+			// Configure and grab a logger so that we can log information
+			// about the creation of the services
+			var provider = services.BuildServiceProvider();		// Build a 'temporary' instance of the DI container
 			var loggerFactory = provider.GetService<ILoggerFactory>();
 
 			var service = factory(configuration, loggerFactory);
@@ -55,11 +70,16 @@ namespace Fritz.StreamTools.StartupServices
 			services.AddSingleton(service as IStreamService);
 		}
 
+		/// <summary>
+		/// Configure the standard ASP.NET Core services
+		/// </summary>
+		/// <param name="services"></param>
 		private static void ConfigureAspNetFeatures(this IServiceCollection services)
 		{
 			services.AddSignalR();
 			services.AddSingleton<FollowerHub>();
 			services.AddMvc();
 		}
+
 	}
 }

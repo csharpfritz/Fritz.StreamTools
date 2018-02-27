7bb428f0dc7bd24ba48c77242a5ea7d0cebbb3c4
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2018-02-27 12:18:29 -0500

    Refactored FollowerGoal  (#54)
    * Moved Gradient to a Helper method
    
    * Refactored to eliminate FollowerGoal and ViewBag usage
    
    * Now using ModelBinding
    
    * Moved logic to FollowerGoalConfiguration

--- Fritz.StreamTools/Models/FollowerGoalConfiguration.cs
+++ Fritz.StreamTools/Models/FollowerGoalConfiguration.cs
@@ -1,40 +1,52 @@
-﻿using System.Linq;
+﻿using Fritz.StreamTools.Helpers;
+using System;
+using System.Linq;
 
 namespace Fritz.StreamTools.Models
 {
-	public class FollowerGoalConfiguration
-	{
-
-		public string Caption { get; set; } = "Follower Goal";
-
-		public int Goal { get; set; } = 1;
-
-		public string EmptyBackgroundColor { get; set; }
-
-		public string EmptyFontColor { get; set; }
-
-		public string FillBackgroundColor { get; set; }
-
-		public string[] FillBgColorArray
+		public class FollowerGoalConfiguration
 		{
-			get
-			{
-				return FillBackgroundColor.Split(',');
-			}
-		}
+				public string Caption { get; set; } = "Follower Goal";
+				public int Goal { get; set; } = 0;
+				public int CurrentValue { get; set; } = 0;
+				public int Width { get; set; } = 800;
+				public string EmptyBackgroundColor { get; set; }
+				public string EmptyFontColor { get; set; }
+				public string FillFontColor { get; set; }
+				public string FontName { get; set; } = "~";
+				public string Gradient { get; set; }
+				public string FillBackgroundColor { get; set; } = "";
+				public string[] FillBgColorArray
+				{
+						get
+						{
+								return FillBackgroundColor.Split(',');
+						}
+				}
+				public string FillBackgroundColorBlend { get; set; } = "0";
+				public double[] FillBgBlendArray
+				{
+						get
+						{
+								return FillBackgroundColorBlend.Split(',').Select(x => double.Parse(x)).ToArray();
+						}
+				}
 
-		public string FillFontColor { get; set; }
-
-		public string FillBackgroundColorBlend { get; set; }
-
-		public double[] FillBgBlendArray
+		internal void LoadDefaultValues(FollowerGoalConfiguration configuration)
 		{
-			get
-			{
-				return FillBackgroundColorBlend.Split(',').Select(x => double.Parse(x)).ToArray();
-			}
-		}
 
-		public string FontName { get; set; }
+			this.Caption = string.IsNullOrEmpty(this.Caption) ? configuration.Caption : this.Caption == "null" ? "" : this.Caption;
+			this.Goal = this.Goal == 0 ? configuration.Goal : this.Goal;
+			var backColors = string.IsNullOrEmpty(this.FillBackgroundColor) ? configuration.FillBgColorArray : this.FillBackgroundColor.Split(',');
+			var backBlend = string.IsNullOrEmpty(this.FillBackgroundColorBlend) ? configuration.FillBgBlendArray : this.FillBackgroundColorBlend.Split(',').Select(a => double.Parse(a)).ToArray();
+
+			this.EmptyBackgroundColor = string.IsNullOrWhiteSpace(this.EmptyBackgroundColor) ? configuration.EmptyBackgroundColor : this.EmptyBackgroundColor;
+			this.EmptyFontColor = string.IsNullOrWhiteSpace(this.EmptyFontColor) ? configuration.EmptyFontColor : this.EmptyFontColor;
+			this.FontName = this.FontName == "~" ? configuration.FontName : this.FontName;
+
+			this.Gradient = DisplayHelper.Gradient(backColors, backBlend, this.Width);
+
+
+		}
 	}
 }

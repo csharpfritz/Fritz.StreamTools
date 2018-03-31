4b55a59dca1c7ae924b1dd409b164237aa24cebc
Jeff Fritz <jeffreytfritz@gmail.com>
2018-03-31 13:48:01 -0400

    Added FritzBot

---
+++ Fritz.Chatbot/Commands/HelpCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Fritz.StreamLib.Core;
+
+namespace Fritz.Chatbot.Commands
+{
+	public class HelpCommand : ICommand
+	{
+		public IChatService ChatService { get; set; }
+
+		public string Name => "help";
+
+		public async Task Execute(params string[] args)
+		{
+			await ChatService.SendMessageAsync("Supported commands: !ping !echo !uptime !quote");
+		}
+	}
+
+}

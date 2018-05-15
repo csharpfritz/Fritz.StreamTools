ca2db882d3b67a1c4d38c499660002945c21a80a
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2018-05-15 12:38:27 -0400

    Began work on CSS for scoreboard widget

--- Fritz.StreamTools/Controllers/GitHubController.cs
+++ Fritz.StreamTools/Controllers/GitHubController.cs
@@ -6,64 +6,78 @@ using System.Threading.Tasks;
 using System.Linq;
 using System;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Fritz.StreamTools.Controllers
 {
   public class GitHubController : Controller
 	{
 		public GitHubController(
-					GitHubClient githubClient,
-					IOptions<GitHubConfiguration> githubConfiguration)
+			IMemoryCache cache,
+			GitHubClient githubClient,
+			IOptions<GitHubConfiguration> githubConfiguration)
 		{
+			this.Cache = cache;
 			_gitHubClient = githubClient;
 			_gitHubConfiguration = githubConfiguration.Value;
 		}
 
-		private readonly GitHubClient _gitHubClient;
+    public IMemoryCache Cache { get; }
+
+    private readonly GitHubClient _gitHubClient;
 		private readonly GitHubConfiguration _gitHubConfiguration;
-	
+
 		public async Task<IActionResult> ContributorsInformation()
 		{
 			var model = new GitHubInformation();
 
-			var repository =
-				await _gitHubClient.Repository.Get(_gitHubConfiguration.RepositoryOwner, _gitHubConfiguration.RepositoryName);
-			var contributors =
-				await _gitHubClient.Repository.Statistics.GetContributors(repository.Id);
-			var lastMonth = DateTimeOffset.Now.AddMonths(-1);
+			model = await Cache.GetOrCreateAsync<GitHubInformation>("GitHubData", async (x) => {
 
-			model.TopEverContributors.AddRange(
-							contributors.Where(c => c.Total > 0)
-													.OrderByDescending(c => c.Total)
-													.Take(5)
-													.Select(c => new GitHubContributor() {
-																				Author = c.Author.Login,
-																				Commits = c.Total
-													}));
+				x.AbsoluteExpiration = DateTime.Now.AddMinutes(5);
 
-			model.TopMonthContributors.AddRange(
-							contributors.OrderByDescending(c => c.Weeks.Where(w => w.Week >= lastMonth)
-																													.Sum(e => e.Commits))
-													.Select(c => new GitHubContributor {
-																				Author = c.Author.Login,
-																				Commits = c.Weeks.Where(w => w.Week >= lastMonth)
-																													.Sum(e => e.Commits)
-													})
-													.Where(c => c.Commits > 0)
-													.OrderByDescending(c => c.Commits)
-													.Take(5));
+				var repository =
+					await _gitHubClient.Repository.Get(_gitHubConfiguration.RepositoryOwner, _gitHubConfiguration.RepositoryName);
+				var contributors =
+					await _gitHubClient.Repository.Statistics.GetContributors(repository.Id);
+				var lastMonth = DateTimeOffset.Now.AddMonths(-1);
 
-			model.TopWeekContributors.AddRange(
-							contributors.Where(c => c.Weeks.Last().Commits > 0)
-													.Select(c => new GitHubContributor {
-																							Author = c.Author.Login,
-																							Commits = c.Weeks.Last().Commits
-													})
-													.Where(c => c.Commits > 0)
-													.OrderByDescending(c => c.Commits)
-													.Take(5));
+				model.TopEverContributors.AddRange(
+								contributors.Where(c => c.Total > 0)
+														.OrderByDescending(c => c.Total)
+														.Take(5)
+														.Select(c => new GitHubContributor() {
+																					Author = c.Author.Login,
+																					Commits = c.Total
+														}));
+
+				model.TopMonthContributors.AddRange(
+								contributors.OrderByDescending(c => c.Weeks.Where(w => w.Week >= lastMonth)
+																														.Sum(e => e.Commits))
+														.Select(c => new GitHubContributor {
+																					Author = c.Author.Login,
+																					Commits = c.Weeks.Where(w => w.Week >= lastMonth)
+																														.Sum(e => e.Commits)
+														})
+														.Where(c => c.Commits > 0)
+														.OrderByDescending(c => c.Commits)
+														.Take(5));
+
+				model.TopWeekContributors.AddRange(
+								contributors.Where(c => c.Weeks.Last().Commits > 0)
+														.Select(c => new GitHubContributor {
+																								Author = c.Author.Login,
+																								Commits = c.Weeks.Last().Commits
+														})
+														.Where(c => c.Commits > 0)
+														.OrderByDescending(c => c.Commits)
+														.Take(5));
+
+				return model;
+
+			});
 
 			return View(model);
+
 		}
 
 		public IActionResult Configuration()

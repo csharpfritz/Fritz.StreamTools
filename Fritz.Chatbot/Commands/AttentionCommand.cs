e87ad3c408b69426d4d503a08cb3c8d4f56036b6
Florian Kahler <f.kahler@online.de>
2018-12-13 18:18:31 +0100

    Fix issue #205

---
+++ Fritz.Chatbot/Commands/AttentionCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Fritz.StreamLib.Core;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Fritz.Chatbot.Commands
+{
+  public class AttentionCommand : IBasicCommand
+  {
+	private readonly IConfiguration Configuration;
+
+	public AttentionCommand(IConfiguration configuration)
+	{
+	  this.Configuration = configuration;
+	}
+
+	public string Trigger => "Fritz";
+
+	public string Description => "Play audio queue to divert attention to chat";
+
+	public TimeSpan? Cooldown => TimeSpan.FromSeconds(5);
+
+	public async Task Execute(IChatService chatService, string userName, ReadOnlyMemory<char> rhs)
+	{
+	  var player = new NetCoreAudio.Player();
+	  await player.Play("hey_listen.wav");
+
+	  var attentionText = Configuration["FritzBot:AttentionCommand:TemplateText"];
+
+	  var sb = new StringBuilder();
+	  sb.AppendFormat(attentionText, userName);
+
+	  await chatService.SendMessageAsync(attentionText);
+	}
+  }
+}

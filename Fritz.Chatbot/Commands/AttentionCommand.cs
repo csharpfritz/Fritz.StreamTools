9ed2fd87c8170bfda1375364aed96feb9c414599
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2018-12-18 13:54:16 -0500

    Fixed AttentionCommand to properly connect and stay connected

--- Fritz.Chatbot/Commands/AttentionCommand.cs
+++ Fritz.Chatbot/Commands/AttentionCommand.cs
@@ -5,19 +5,28 @@ using Fritz.StreamLib.Core;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Fritz.Chatbot.Commands
 {
-	public class AttentionCommand : IBasicCommand
+	public class AttentionCommand : IBasicCommand., IDisposable
 	{
 		private readonly IConfiguration Configuration;
 
-		public AttentionCommand(IConfiguration configuration)
+		public ILogger Logger { get; }
+
+		public AttentionCommand(IConfiguration configuration, ILoggerFactory loggerFactory)
 		{
 			this.Configuration = configuration;
+			this.Logger = loggerFactory.CreateLogger("AttentionCommand");
+
 			var thisUri = new Uri(configuration["FritzBot:ServerUrl"], UriKind.Absolute);
-			this.Client = new HubConnectionBuilder().WithUrl(new Uri(thisUri, "attentionhub").ToString()).Build();
-			this.Client.StartAsync();
+			var attentionUri = new Uri(thisUri, "attentionhub");
+
+			Logger.LogTrace($"Connecting AttentionCommand to: {attentionUri}");
+
+			this.Client = new HubConnectionBuilder().WithUrl(attentionUri.ToString()).Build();
+
 		}
 
 		protected HubConnection Client { get; }
@@ -30,11 +39,19 @@ namespace Fritz.Chatbot.Commands
 
 		public async Task Execute(IChatService chatService, string userName, ReadOnlyMemory<char> rhs)
 		{
+
+			await this.Client.StartAsync();
+
 			await this.Client.InvokeAsync("AlertFritz");
 
 			var attentionText = Configuration["FritzBot:AttentionCommand:TemplateText"];
 
 			await chatService.SendMessageAsync(string.Format(attentionText, userName));
+		}
+
+		public void Dispose()
+		{
+			Client.DisposeAsync();
 		}
 	}
 }

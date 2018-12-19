6203b40cbde9f88504c50492da6a03fc49839df9
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2018-12-19 14:17:30 -0500

    Now using HubContext for triggering the Attention command #211

--- Fritz.Chatbot/Commands/AttentionCommand.cs
+++ Fritz.Chatbot/Commands/AttentionCommand.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using System.Threading.Tasks;
 using Fritz.StreamLib.Core;
+using Fritz.StreamTools.Hubs;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,49 +11,50 @@ using Microsoft.Extensions.Logging;
 
 namespace Fritz.Chatbot.Commands
 {
-	public class AttentionCommand : IBasicCommand, IDisposable
+	public class AttentionCommand : IBasicCommand
 	{
 		private readonly IConfiguration Configuration;
 
 		public ILogger Logger { get; }
+		public IHubContext<AttentionHub, IAttentionHubClient> HubContext { get; }
 
-		public AttentionCommand(IConfiguration configuration, ILoggerFactory loggerFactory)
+		public AttentionCommand(IConfiguration configuration, IHubContext<AttentionHub, IAttentionHubClient> hubContext, ILoggerFactory loggerFactory)
 		{
 			this.Configuration = configuration;
 			this.Logger = loggerFactory.CreateLogger("AttentionCommand");
 
-			var thisUri = new Uri(configuration["FritzBot:ServerUrl"], UriKind.Absolute);
-			var attentionUri = new Uri(thisUri, "attentionhub");
+			this.HubContext = hubContext;
 
-			Logger.LogTrace($"Connecting AttentionCommand to: {attentionUri}");
+			//var thisUri = new Uri(configuration["FritzBot:ServerUrl"], UriKind.Absolute);
+			//var attentionUri = new Uri(thisUri, "attentionhub");
 
-			this.Client = new HubConnectionBuilder().WithUrl(attentionUri.ToString()).Build();
+			//Logger.LogTrace($"Connecting AttentionCommand to: {attentionUri}");
+
+			//this.Client = new HubConnectionBuilder().WithUrl(attentionUri.ToString()).Build();
 
 		}
 
-		protected HubConnection Client { get; }
+		//protected HubConnection Client { get; }
 
 		public string Trigger => "attention";
 
 		public string Description => "Play audio queue to divert attention to chat";
 
+#if DEBUG
+		public TimeSpan? Cooldown => TimeSpan.FromSeconds(10);
+#else
 	public TimeSpan? Cooldown => TimeSpan.Parse(Configuration["FritzBot:AttentionCommand:Cooldown"]);
+#endif
 
 		public async Task Execute(IChatService chatService, string userName, ReadOnlyMemory<char> rhs)
 		{
 
-			await this.Client.StartAsync();
-
-			await this.Client.InvokeAsync("AlertFritz");
+			await this.HubContext.Clients.All.AlertFritz();
 
 			var attentionText = Configuration["FritzBot:AttentionCommand:TemplateText"];
 
 			await chatService.SendMessageAsync(string.Format(attentionText, userName));
 		}
 
-		public void Dispose()
-		{
-			Client.DisposeAsync();
-		}
 	}
 }

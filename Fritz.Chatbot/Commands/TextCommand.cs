2627b002a9680137905ca344b45ada5daf981a84
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-03-05 18:57:27 -0500

    Updated to integrate the TextCommand better

--- Fritz.Chatbot/Commands/TextCommand.cs
+++ Fritz.Chatbot/Commands/TextCommand.cs
@@ -18,7 +18,7 @@ namespace Fritz.Chatbot.Commands
 		public bool Final => true;
 		public TimeSpan? Cooldown => TimeSpan.FromSeconds(5);
 
-		private static readonly Dictionary<string, string> _Commands = new Dictionary<string, string>
+		internal static readonly Dictionary<string, string> _Commands = new Dictionary<string, string>
 		{
 			{ "music", "Jeff plays 'Music to Code By' from Carl Franklin: http://mtcb.pwop.com" },
 			{ "discord", "Join us on the Fritz and Friends Discord server at: https://discord.gg/RnJhrJq" },
@@ -26,9 +26,16 @@ namespace Fritz.Chatbot.Commands
 			{ "keyboard", "Jeff uses a Vortex Race 3 with Cherry MX Blue switches, details on his blog at: https://jeffreyfritz.com/2018/07/mechanical-keyboards-i-just-got-one-and-why-you-need-one-too/"  },
 			{ "blog", "Jeff's blog is at: https://jeffreyfritz.com" },
 			{ "lurk", "is stepping away from keyboard and lurking" },
-			{ "raid", @"Prepare to RAID!  Copy this text to use when we reach our raid target: ----------------------------------------------- csharpRaid csharpRaid csharpRaid CsharpFritz's Coding Squad is Here! All your base are belong to us! csharpRaid csharpRaid csharpRaid" +
-				"----------------------------------------------- twitchRaid twitchRaid twitchRaid CsharpFritz's Coding Squad is Here! All your base are belong to us! twitchRaid twitchRaid twitchRaid  " }
+			{ "raid", @"Prepare to RAID!  Copy this text to use when we reach our raid target: ------------ Subscribers copy -------------\n\ncsharpRaid csharpRaid csharpRaid CsharpFritz's Coding Squad is Here! All your base are belong to us! csharpRaid csharpRaid csharpRaid\n" +
+				" ------------ Non-Subscribers copy -------------\n\n twitchRaid twitchRaid twitchRaid CsharpFritz's Coding Squad is Here! All your base are belong to us! twitchRaid twitchRaid twitchRaid  " }
 		};
+
+		internal static bool IsCommand(string commandText)
+		{
+
+			return _Commands.ContainsKey(commandText);
+
+		}
 
 		public bool CanExecute(string userName, string fullCommandText)
 		{

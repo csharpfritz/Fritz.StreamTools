783648f484072f06fc27ce294740b2735059132f
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-03-24 13:33:20 -0400

    Added YouTube command

--- Fritz.Chatbot/Commands/TextCommand.cs
+++ Fritz.Chatbot/Commands/TextCommand.cs
@@ -25,6 +25,7 @@ namespace Fritz.Chatbot.Commands
 			{ "github", "Checkout Jeff's GitHub at: https://github.com/csharpfritz and the FritzAndFriends GitHub organization at: https://github.com/FritzAndFriends" },
 			{ "keyboard", "Jeff uses a Vortex Race 3 with Cherry MX Blue switches, details on his blog at: https://jeffreyfritz.com/2018/07/mechanical-keyboards-i-just-got-one-and-why-you-need-one-too/"  },
 			{ "blog", "Jeff's blog is at: https://jeffreyfritz.com" },
+			{ "youtube", "Find the archive of videos from our channel at:  https://youtube.com/csharpfritz" }
 			{ "lurk", "is stepping away from keyboard and lurking" },
 			{ "defend", "csharpNo csharpGritty We shall defend the channel! csharpNo csharpGritty" },
 			{ "raid", @"Prepare to RAID!  Copy this text to use when we reach our raid target: ------------ Subscribers copy ------------- csharpRaid csharpRaid csharpRaid CsharpFritz's Coding Squad is Here! All your base are belong to us! csharpRaid csharpRaid csharpRaid " +

3adb385cd11997f843d4ac0ca550377163286f63
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-03-24 12:25:16 -0400

    Added some text commands and the old man dev channel points feature

--- Fritz.StreamTools/Services/TwitchPubSubService.cs
+++ Fritz.StreamTools/Services/TwitchPubSubService.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,14 +22,27 @@ namespace Fritz.StreamTools.Services
 		private IServiceProvider _ServiceProvider;
 		private CancellationToken _Token;
 		private Task _BackgroundTask;
+		private static string[] _OldManSoundFx;
+		public static short _OldManCount = 0;
 		private readonly Twitch.PubSub.Proxy _Proxy;
 		private readonly ConfigurationSettings _Configuration;
+		private readonly IHostEnvironment _HostEnvironment;
+		private readonly Dictionary<string, Action<IHubContext<AttentionHub, IAttentionHubClient>>> _ChannelPointActions = new Dictionary<string, Action<IHubContext<AttentionHub, IAttentionHubClient>>>();
 
-		public TwitchPubSubService(IServiceProvider serviceProvider, Twitch.PubSub.Proxy proxy, IOptions<ConfigurationSettings> settings)
+		public TwitchPubSubService(IServiceProvider serviceProvider, Twitch.PubSub.Proxy proxy, IHostEnvironment hostEnvironment, IOptions<ConfigurationSettings> settings)
 		{
 			_ServiceProvider = serviceProvider;
 			_Proxy = proxy;
 			_Configuration = settings.Value;
+			_HostEnvironment = hostEnvironment;
+
+			InitializeChannelPointActions();
+
+		}
+
+		private void InitializeChannelPointActions()
+		{
+			_ChannelPointActions.Add("get off my lawn!", (c) => TwitchPubSubService.OldManDeveloper(c));
 		}
 
 		public Task StartAsync(CancellationToken cancellationToken)
@@ -36,7 +50,17 @@ namespace Fritz.StreamTools.Services
 			_Proxy.OnChannelPointsRedeemed += _Proxy_OnChannelPointsRedeemed;
 			_Token = cancellationToken;
 			_BackgroundTask = _Proxy.StartAsync(new TwitchTopic[] { TwitchTopic.ChannelPoints(_Configuration.UserId) }, _Token);
+			IdentifyOldManAudio();
 			return Task.CompletedTask;
+		}
+
+		private void IdentifyOldManAudio()
+		{
+
+			var oldManPath = Path.Combine(_HostEnvironment.ContentRootPath, "wwwroot", "contents", "oldman");
+			var di = new DirectoryInfo(oldManPath);
+			_OldManSoundFx = di.GetFiles().Select(f => f.Name).OrderBy(x => Guid.NewGuid()).ToArray();
+
 		}
 
 		private void _Proxy_OnChannelPointsRedeemed(object sender, ChannelRedemption e)
@@ -44,20 +68,28 @@ namespace Fritz.StreamTools.Services
 			using (var scope = _ServiceProvider.CreateScope())
 			{
 				var context = scope.ServiceProvider.GetRequiredService<IHubContext<AttentionHub, IAttentionHubClient>>();
-				//context.Clients.All.PlaySoundEffect("pointsredeemed.mp3");
-				var redemption = new ChannelPointRedemption
+
+				if (_ChannelPointActions.ContainsKey(e.redemption.reward.title.ToLowerInvariant())) {
+					_ChannelPointActions[e.redemption.reward.title.ToLowerInvariant()](context);
+				}
+				else
 				{
-					RedeemingUserName = e.redemption.user.display_name,
-					RedeemingUserId = e.redemption.user.id,
-					RewardName = e.redemption.reward.title,
-					RewardValue = e.redemption.reward.cost,
-					RewardPrompt = e.redemption.user_input,
-					BackgroundColor = e.redemption.reward.background_color,
-					Image_1x = new Uri(e.redemption.reward.image?.url_1x ?? e.redemption.reward.default_image.url_1x),
-					Image_2x = new Uri(e.redemption.reward.image?.url_2x ?? e.redemption.reward.default_image.url_2x),
-					Image_4x = new Uri(e.redemption.reward.image?.url_4x ?? e.redemption.reward.default_image.url_4x)
-				};
-				context.Clients.All.NotifyChannelPoints(redemption);
+
+					var redemption = new ChannelPointRedemption
+					{
+						RedeemingUserName = e.redemption.user.display_name,
+						RedeemingUserId = e.redemption.user.id,
+						RewardName = e.redemption.reward.title,
+						RewardValue = e.redemption.reward.cost,
+						RewardPrompt = e.redemption.user_input,
+						BackgroundColor = e.redemption.reward.background_color,
+						Image_1x = new Uri(e.redemption.reward.image?.url_1x ?? e.redemption.reward.default_image.url_1x),
+						Image_2x = new Uri(e.redemption.reward.image?.url_2x ?? e.redemption.reward.default_image.url_2x),
+						Image_4x = new Uri(e.redemption.reward.image?.url_4x ?? e.redemption.reward.default_image.url_4x)
+					};
+					context.Clients.All.NotifyChannelPoints(redemption);
+
+				}
 			}
 		}
 
@@ -65,6 +97,20 @@ namespace Fritz.StreamTools.Services
 		{
 			_Proxy.Dispose();
 			return Task.CompletedTask;
+
 		}
+
+		#region Channel Point Actions
+
+		public static void OldManDeveloper(IHubContext<AttentionHub, IAttentionHubClient> hubContext)
+		{
+
+			var fx = _OldManSoundFx[_OldManCount++ % _OldManSoundFx.Length];
+			hubContext.Clients.All.PlaySoundEffect($"oldman/{fx}");
+
+		}
+
+		#endregion
+
 	}
 }

177c55c116d179fe952b4785515f81cb0ffaebe8
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-06-29 19:50:09 -0400

    Completed implementation of the ToDo widget

--- Fritz.Chatbot/Commands/ToDoCommand.cs
+++ Fritz.Chatbot/Commands/ToDoCommand.cs
@@ -3,6 +3,7 @@ using Fritz.StreamTools.Hubs;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace Fritz.Chatbot.Commands
 	public class ToDoCommand : IBasicCommand2
 	{
 		public string Trigger => "todo";
-		public string Description => "Manage the to-do list displayed";
+		public string Description => "Manage the to-do list displayed.  !todo add <text> to add an item, !todo done <id> to mark an item completed, !todo remove <id> to remove an item";
 		public TimeSpan? Cooldown => TimeSpan.FromSeconds(5);
 
 		private static Dictionary<int, (bool completed, string text)> _ToDos = new Dictionary<int, (bool, string)>();
@@ -29,13 +30,25 @@ namespace Fritz.Chatbot.Commands
 
 			var arrArgs = rhs.ToString().Split(' ');
 			if (arrArgs[0] == "add") {
-				_ToDos.Add(_ToDos.Count + 1, (false, rhs.ToString().Substring(4).Trim()));
-				await _HubContext.Clients.All.SendAsync("todo_new", _ToDos.Count, _ToDos[_ToDos.Count].text);
-			} else if (arrArgs[0] == "done" && int.TryParse(arrArgs[1], out var id)) {
+				var newKey = !_ToDos.Any() ? 1 : _ToDos.Max(t => t.Key) + 1;
+				_ToDos.Add(newKey, (false, rhs.ToString().Substring(4).Trim()));
+				await _HubContext.Clients.All.SendAsync("todo_new", newKey, _ToDos[newKey].text);
+			}
+			else if (arrArgs[0] == "remove" && int.TryParse(arrArgs[1], out var removeid) && _ToDos.Any(t => t.Key == removeid))
+			{
+				_ToDos.Remove(removeid);
+				await _HubContext.Clients.All.SendAsync("todo_remove", removeid);
+			}
+			else if (arrArgs[0] == "done" && int.TryParse(arrArgs[1], out var id) && _ToDos.Any(t => t.Key == id))
+			{
 				var todo = _ToDos[id];
 				todo.completed = true;
 				_ToDos[id] = todo;
 				await _HubContext.Clients.All.SendAsync("todo_done", id);
+			}
+			else if (arrArgs[0] == "speed" && float.TryParse(arrArgs[1], out var speed))
+			{
+				await _HubContext.Clients.All.SendAsync("todo_speed", speed);
 			}
 
 		}
